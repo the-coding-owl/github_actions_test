e2e83ab805be08f34e88eb424f6b776f24819f39
Kevin Ditscheid <kevin@the-coding-owl.de>
2020-03-08 15:16:28 +0100

    [INIT] Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+
+$EM_CONF['github_actions_test'] = array(
+	'title' => 'Github Actions Test',
+	'description' => 'Just an empty shell to test github actions',
+	'category' => 'plugin',
+	'author' => 'Kevin Ditscheid',
+	'author_email' => 'kevin@the-coding-owl.de',
+	'state' => 'experimental',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '10.3.0-10.3.99'
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		)
+	)
+);
